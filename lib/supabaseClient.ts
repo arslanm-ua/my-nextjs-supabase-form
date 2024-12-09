b998ff340ea98137c6a115052fdf2f21443b9433
arslanm-ua <uarslanm@gmail.com>
2024-12-10 00:55:52 +0100

    supabase throw

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -3,4 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 
+if (!supabaseUrl) throw new Error('supabaseUrl is required.')
+    if (!supabaseKey) throw new Error('supabaseKey is required.')
+
 export const supabase = createClient(supabaseUrl, supabaseKey)
